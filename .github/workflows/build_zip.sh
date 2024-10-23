097a98abacb72438b4ec1d57e1c45747ee6611e6
AlexKnauth <alexander@knauth.org>
2024-10-23 03:34:55 -0400

    Create github workflows ci.yml

---
+++ .github/workflows/build_zip.sh
@@ -0,0 +1,27 @@
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    if [[ $OS_NAME =~ ^macos\-.*$ ]]; then
+        stage=$(mktemp -d -t tmp)
+    else
+        stage=$(mktemp -d)
+    fi
+
+    cp "src-tauri/target/release/socd-cross.exe" "$stage/socd-cross.exe" 2>/dev/null || :
+    cp "src-tauri/target/release/socd-cross" "$stage/socd-cross" 2>/dev/null || :
+
+    cd $stage
+    if [ "$OS_NAME" = "windows-latest" ]; then
+        7z a $src/socd-cross-$TARGET.zip *
+    else
+        tar czf $src/socd-cross-$TARGET.tar.gz *
+    fi
+    cd $src
+
+    rm -rf $stage
+}
+
+main
