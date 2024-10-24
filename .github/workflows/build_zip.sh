7a0fcaf6b39364b6fc3a55392b77681490ee8c8b
AlexKnauth <alexander@knauth.org>
2024-10-24 00:49:21 -0400

    Bundle MacOS App

--- .github/workflows/build_zip.sh
+++ .github/workflows/build_zip.sh
@@ -12,6 +12,7 @@ main() {
 
     cp "src-tauri/target/release/socd-cross.exe" "$stage/socd-cross.exe" 2>/dev/null || :
     cp "src-tauri/target/release/socd-cross" "$stage/socd-cross" 2>/dev/null || :
+    cp -R "src-tauri/target/release/bundle/macos/socd-cross.app" "$stage/socd-cross.app" 2>/dev/null || :
 
     cd $stage
     if [ "$OS_NAME" = "windows-latest" ]; then
